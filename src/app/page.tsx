e48a18fa5e5c0e11d0e0127b8144bf3ac9ed1ea4
SukritiR29 <sukritirajora@gmail.com>
2023-11-17 18:34:51 +0530

    dashboard update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/home/hero/Hero";
 export default function Home() {
   return (
     <>
-      <Navbar />
+      
       <Hero />
     </>
   );
--- src/components/home/hero/Hero.tsx
+++ src/components/home/hero/Hero.tsx
@@ -1,53 +1,59 @@
 "use client";
 import Link from 'next/link';
+import Navbar from "@/components/global/Navbar";
 import React, { useEffect, useRef } from "react";
-import HeroChart from "./HeroChart";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { signIn, useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Hero() {
+
+  const bg = 'url("/images/home/Qarebg.png")';
+  const style = {
+    backgroundImage: bg, backgroundSize: 'cover',  backgroundPosition: 'center', minHeight: '100vh',  
+  };
+  const session = useSession();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (session.status === "authenticated") {
+      // router.push("/profile");
+    }
+  });
+  
   return (
-    <div className="w-full max-w-[75rem] mx-auto flex items-center justify-center flex-col gap-4">
-      <h1 className="text-4xl font-bold">
-        Unlock QR <span className="text-primary">Insights</span>
-      </h1>
-      <div className="w-full max-w-2xl bg-accent rounded-2xl p-4">
-        <div className="">
-          <HeroChart />
-        </div>
-        <div className="flex gap-4">
-          <Image
-            src={"/images/home/qr.png"}
-            height={128}
-            width={128}
+    <div className="hero min-h-screen" style={style}>
+    <div className=''>
+    <div className='flex justify-center items-center'>
+    <Image
+            src={"/logo.png"}
+            height={100}
+            width={100}
             alt="qare"
+            className='text-center mt-20'
           />
-          <div className="rounded-xl w-full flex flex-col p-2 ring-primary ring-1">
-            <div className="">
-              <h2 className="text-xl font-medium">QAre Link</h2>
-              <a href="https://qare.anirudhsoni.in" className="text-blue-800">
-                https://qare.anirudhsoni.in/qr?sdoiu3r
-              </a>
-            </div>
-            <div className="">
-              <h2 className="text-xl font-medium">Clicks</h2>
-              <p>200</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex items-center justify-center w-full max-w-xl flex-col gap-4">
-        <p className="text-center">
-          QAre leverages QR codes for enhanced marketing. It generates trackable
-          codes, bridging physical and digital realms, empowering insightful
-          campaigns.
-        </p>
-        <Link href="/dashboard">
-          <Button>
-           Get Started
-          </Button>
-        </Link>
-      </div>
+    </div>
+    <div className='flex justify-center items-center mt-10'>
+      <p className='text-center'>QAre leverages QR codes for enhanced marketing.
+         It generates trackable codes,  <br /> bridging physical and digital realms, empowering insightful campaigns.</p>
+    </div>
+    <div className='flex justify-center items-center mt-10'>
+    {session.status === "authenticated" && (
+        <Button
+          onClick={() => {
+            router.push("/dashboard");
+          }}
+        >
+          Dashboard
+        </Button>
+      )}
+      {session.status === "unauthenticated" && (
+        <Button onClick={() => signIn("google")}>Login</Button>
+      )}
+      {session.status === "loading" && <Button>Loading..</Button>}
+    </div>
+    </div>
     </div>
   );
 }
